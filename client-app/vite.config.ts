bc84cad98e52dc978a3aa2e4c97c4f2bb5003917
ryotaro-tanaka <ryotaro_tanaka@outlook.com>
2025-04-08 12:20:25 +1000

    resolve docker

--- client-app/vite.config.ts
+++ client-app/vite.config.ts
@@ -5,6 +5,10 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config https://vitest.dev/config
 export default defineConfig({
+  server: {
+    host: true, // 必須: 外部からアクセス可能にする
+    port: 5173,
+  },
   plugins: [react(), tsconfigPaths()],
   test: {
     globals: true,
