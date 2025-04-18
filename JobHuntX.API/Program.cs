2e3e19b79ca65cfb59ac42bdb3a2299b196c1a77
ryotaro-tanaka <ryotaro_tanaka@outlook.com>
2025-04-18 22:43:41 +1000

    .editorconfig + dotnet-format

--- JobHuntX.API/Program.cs
+++ JobHuntX.API/Program.cs
@@ -5,7 +5,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddSwaggerGen();
 
 // Add CORS policy
 var allowedOrigins = builder.Configuration.GetSection("AllowedOrigins").Get<string[]>();
