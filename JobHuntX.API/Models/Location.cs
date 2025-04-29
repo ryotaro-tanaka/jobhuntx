347d4d292e606bb76e1850c1e649a10eb4da60f5
ryotaro-tanaka <ryotaro_tanaka@outlook.com>
2025-04-29 11:56:55 +1000

    LocationType type

--- JobHuntX.API/Models/Location.cs
+++ JobHuntX.API/Models/Location.cs
@@ -1,7 +1,13 @@
 namespace JobHuntX.API.Models;
 
 public class Location {
-    public string? Type { get; set; } // e.g., "Remote"
+    public LocationType Type { get; set; } // Limited to Remote, Hybrid, Onsite
     public string? City { get; set; } // e.g., "Sydney"
     public string? Country { get; set; } // e.g., "Australia"
+}
+
+public enum LocationType {
+    Remote,  // リモート
+    Hybrid,  // ハイブリッド
+    Onsite   // オンサイト
 }
