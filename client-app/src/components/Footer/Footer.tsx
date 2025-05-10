64dbbded66031d4323896c28dff88f527fad19da
ryotaro-tanaka <ryotaro_tanaka@outlook.com>
2025-05-10 18:43:04 +1000

    make Footer responsive

--- client-app/src/components/Footer/Footer.tsx
+++ client-app/src/components/Footer/Footer.tsx
@@ -1,15 +1,15 @@
 import githubLogo from '../../assets/github.svg';
 
 const Footer = () => (
-  <footer className="w-full bg-gray-100 py-4 text-sm text-gray-500 flex items-center justify-center relative">
-    <span className="mx-auto">
+  <footer className="w-full bg-gray-100 py-4 text-sm text-gray-500 flex flex-col md:flex-row items-center justify-center relative">
+    <span className="mx-auto md:mx-0">
       © {new Date().getFullYear()} Ryotaro Tanaka. All rights reserved.
     </span>
     <a
       href="https://github.com/ryotaro-tanaka/jobhuntx"
       target="_blank"
       rel="noopener noreferrer"
-      className="absolute right-8 inline-flex items-center gap-1 text-gray-600 hover:text-black"
+      className="mt-2 md:mt-0 md:absolute md:right-8 inline-flex items-center gap-1 text-gray-600 hover:text-black"
     >
       <img src={githubLogo} alt="GitHub" width={20} height={20} />
     </a>
